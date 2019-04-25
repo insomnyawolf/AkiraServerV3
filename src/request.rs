92a69738fd9086b5e2406a3d838bf55d3fe11230
insomnyawolf <insomnyawolf@gmail.com>
2019-04-25 07:55:43 +0200

    Add Response Headers And Basic Documentation

--- src/request.rs
+++ src/request.rs
@@ -7,6 +7,7 @@ pub struct Request {
 }
 
 impl Request {
+    /** Parse request and headers from byte buffer **/
     pub fn parse(buffer: &[u8]) -> Request {
         let mut req = Request::default();
 
@@ -34,12 +35,14 @@ impl Request {
         Method::from_str(&self.method)
     }
 
+    /** Obtains resource path relative to the specified location **/
     pub fn get_local_path(&self, root_folder: &String) -> String {
         root_folder.to_string() + &self.path
     }
 }
 
 // https://tools.ietf.org/html/rfc2616#section-5.1.1
+/** Enum that contains references to possible request methods **/
 #[derive(Debug, PartialEq)]
 pub enum Method {
     CONNECT,
@@ -53,6 +56,7 @@ pub enum Method {
 }
 
 impl Method {
+    /** Parse Method from String **/
     pub fn from_str(s: &String) -> Option<Method> {
         let string: &str = &s[..];
         match string {
@@ -68,6 +72,7 @@ impl Method {
         }
     }
 
+    /** Converts Method to string **/
     pub fn as_str(&self) -> &str {
         match *self {
             Method::CONNECT => "CONNECT",
@@ -87,93 +92,89 @@ impl Method {
 #[derive(Debug, Default)]
 pub struct RequestHeaders {
     // Standard request fields
-
-    // Acceptable instance-manipulations for the request
+    /** Acceptable instance-manipulations for the request **/
     pub acceptable_instance_manipulations: String,
-    // Media type(s) that is/are acceptable for the response
+    /** Media type(s) that is/are acceptable for the response **/
     pub accept: Vec<String>,
-    // Character sets that are acceptable
+    /** Character sets that are acceptable **/
     pub accept_charset: String,
-    // List of acceptable encodings
+    /** List of acceptable encodings **/
     pub accept_encoding: Vec<String>,
-    // List of acceptable human languages for response
+    /** List of acceptable human languages for response **/
     pub accept_language: String,
-    // Acceptable version in time
+    /** Acceptable version in time **/
     pub accept_datetime: String,
-    // Initiates a request for cross-origin resource sharing with Origin
+    /** Initiates a request for cross-origin resource sharing with Origin **/
     pub access_control_request_method: String,
-    // Authentication credentials for HTTP authentication
+    /** Authentication credentials for HTTP authentication **/
     pub authorization: String,
-    // Used to specify directives that must be obeyed by all caching mechanisms along the request-response chain
+    /** Used to specify directives that must be obeyed by all caching mechanisms along the request-response chain **/
     pub cache_control: String,
-    // Control options for the current connection and list of hop-by-hop request fields
+    /** Control options for the current connection and list of hop-by-hop request fields **/
     pub connection: String,
-    // The length of the request body in octets (8-bit bytes)
+    /** The length of the request body in octets (8-bit bytes) **/
     pub content_length: u64,
-    // A Base64-encoded binary MD5 sum of the content of the request body
+    /** A Base64-encoded binary MD5 sum of the content of the request body **/
     pub content_md5: String,
-    // The Media type of the body of the request (used with POST and PUT requests)
+    /** The Media type of the body of the request (used with POST and PUT requests) **/
     pub content_type: String,
-    // An HTTP cookie previously sent by the server with Set-Cookie (below)
+    /** An HTTP cookie previously sent by the server with Set-Cookie (below) **/
     pub cookie: String,
-    // The date and time at which the message was originated
+    /** The date and time at which the message was originated **/
     pub date: String,
-    // Indicates that particular server behaviors are required by the client
+    // Indicates that particular server behaviors are required by the client **/
     pub expect: String,
-    // Disclose original information of a client connecting to a web server through an HTTP proxy
+    /** Disclose original information of a client connecting to a web server through an HTTP proxy **/
     pub forwarded: String,
-    // The email address of the user making the request
+    /** The email address of the user making the request **/
     pub from: String,
-    // The domain name of the server (for virtual hosting), and the TCP port number on which the server is listening
+    /** The domain name of the server (for virtual hosting), and the TCP port number on which the server is listening **/
     pub host: String,
-    /** ToDo Omited
+    /* ToDo Omited
     HTTP2-Settings
     If-Match
     If-Modified-Since
     If-None-Match
     If-Range
     If-Unmodified-Since
-    **/
-    // Limit the number of times the message can be forwarded through proxies or gateways
+    */
+    /** Limit the number of times the message can be forwarded through proxies or gateways **/
     pub max_forwards: String,
-    // Initiates a request for cross-origin resource sharing (asks server for Access-Control-* response fields)
+    /** Initiates a request for cross-origin resource sharing (asks server for Access-Control-* response fields) **/
     pub origin: String,
-    // Implementation-specific fields that may have various effects anywhere along the request-response chain
+    /** Implementation-specific fields that may have various effects anywhere along the request-response chain **/
     pub pragma: String,
-    // Authorization credentials for connecting to a proxy
+    /** Authorization credentials for connecting to a proxy **/
     pub proxy_authorization: String,
-    // Request only part of an entity. Bytes are numbered from 0
+    /** Request only part of an entity. Bytes are numbered from 0 **/
     pub range: String,
-    // This is the address of the previous web page from which a link to the currently requested page was followed (misspelled in the RFC)
+    /** This is the address of the previous web page from which a link to the currently requested page was followed (misspelled in the RFC) **/
     pub referer: String,
-    // The transfer encodings the user agent is willing to accept:
-    // the same values as for the response header field Transfer-Encoding can be used,
-    // plus the "trailers" value (related to the "chunked" transfer method)
-    // to notify the server it expects to receive additional fields in the trailer after the last, zero-sized, chunk
+    /** The transfer encodings the user agent is willing to accept:
+    the same values as for the response header field Transfer-Encoding can be used,
+    plus the "trailers" value (related to the "chunked" transfer method)
+    to notify the server it expects to receive additional fields in the trailer after the last, zero-sized, chunk **/
     pub transfer_encodings: String,
-    // user agent string
+    /** user agent string **/
     pub user_agent: String,
 
-    /** ToDo Omited
-    Upgrade
-    **/
-    // Informs the server of proxies through which the request was sent
+    // ToDo Omited Upgrade
+    /** Informs the server of proxies through which the request was sent **/
     pub via: String,
-    // A general warning about possible problems with the entity body
+    /** A general warning about possible problems with the entity body **/
     pub warning: String,
 
     // End Of Std Fields
 
     // Common non-standard request fields
-
-    // HTTP Protocol used version
+    /** HTTP Protocol used version **/
     pub version: String,
-    // Tells a server which (presumably in the middle of a HTTP -> HTTPS migration) hosts mixed content that the client would prefer redirection to HTTPS
+    /** Tells a server which (presumably in the middle of a HTTP -> HTTPS migration) hosts mixed content that the client would prefer redirection to HTTPS **/
     pub upgrade_insecure_requests: String,
-    // Requests a web application to disable their tracking of a user
+    /** Requests a web application to disable their tracking of a user **/
     pub dnt: String,
 
-    // Undefined headers
+    /** Undefined headers **/
     pub other: Vec<String>,
 }
 
@@ -181,6 +182,7 @@ impl RequestHeaders {
     pub fn parse(&mut self, client_str: &str) {
         let client_arr: Vec<&str> = client_str.rsplit("\r\n").collect();
 
+        // ToDo Improove this loop
         for data in client_arr {
             let current = data.to_string();
 
@@ -188,7 +190,10 @@ impl RequestHeaders {
                 self.acceptable_instance_manipulations =
                     current.trim_start_matches("A-IM: ").to_string();
             } else if current.starts_with("Accept: ") {
-                let arr: Vec<&str> = current.trim_start_matches("Accept: ").split(" ").collect();
+                // Todo Check This
+                let values = current.trim_start_matches("Accept: ").replace(";", ",");
+                let arr: Vec<&str> = values.split(",").collect();
+
                 for data in arr {
                     self.accept.push(data.to_string());
                 }
--- src/response.rs
+++ src/response.rs
@@ -121,3 +121,111 @@ impl HttpStatus {
         }
     }
 }
+
+// https://en.wikipedia.org/wiki/List_of_HTTP_header_fields
+#[derive(Debug, Default)]
+pub struct ResponseHeaders {
+    // Standard response fields
+    /** Specifying which web sites can participate in cross-origin resource sharing ( * means any ) **/
+    pub access_control_allow_origin: String,
+    /** Specifies which patch document formats this server supports **/
+    pub accept_patch: String,
+    /** What partial content range types this server supports via byte serving **/
+    pub accept_ranges: String,
+    /** The age the object has been in a proxy cache in seconds **/
+    pub age: String,
+    /** Valid methods for a specified resource. To be used for a 405 Method not allowed **/
+    pub allow: String,
+
+    // Todo Omited Alt-Svc
+    /** Tells all caching mechanisms from server to client whether they may cache this object. It is measured in seconds **/
+    pub cache_control: String,
+    /** Control options for the current connection and list of hop-by-hop response fields. **/
+    pub connection: String,
+    /** An opportunity to raise a "File Download" dialogue box for a known MIME type with binary format or suggest a filename for dynamic content.
+    Quotes are necessary with special characters. **/
+    pub content_disposition: String,
+    /** The type of encoding used on the data. **/
+    pub content_encoding: String,
+    /** The natural language or languages of the intended audience for the enclosed content **/
+    pub content_language: String,
+    /** The length of the response body in octets (8-bit bytes) **/
+    pub content_length: String,
+    /** An alternate location for the returned data **/
+    pub content_location: String,
+    /** A Base64-encoded binary MD5 sum of the content of the response **/
+    pub content_md5: String,
+    /** Where in a full body message this partial message belongs **/
+    pub content_range: String,
+    /** The MIME type of this content **/
+    pub content_type: String,
+    /** The date and time that the message was sent **/
+    pub date: String,
+    /** Specifies the delta-encoding entity tag of the response **/
+    pub delta_base: String,
+    /** An identifier for a specific version of a resource, often a message digest **/
+    pub e_tag: String,
+    /** Gives the date/time after which the response is considered stale **/
+    pub expires: String,
+    /** Instance-manipulations applied to the response **/
+    pub instance_manipulations: String,
+    /** The last modified date for the requested object **/
+    pub last_modified: String,
+    /** Used to express a typed relationship with another resource **/
+    pub link: String,
+    /** Used in redirection, or when a new resource has been created. **/
+    pub location: String,
+    /** This field is supposed to set P3P policy, in the form of P3P:CP="your_compact_policy".
+    However, P3P did not take off, most browsers have never fully implemented it,
+    a lot of websites set this field with fake policy text,
+    that was enough to fool browsers the existence of P3P policy and grant permissions for third party cookies. **/
+    pub p3p: String,
+    /** Implementation-specific fields that may have various effects anywhere along the request-response chain. **/
+    pub pragma: String,
+    /** Request authentication to access the proxy **/
+    pub proxy_authenticate: String,
+    /** HTTP Public Key Pinning, announces hash of website's authentic TLS certificate **/
+    pub public_key_pins: String,
+    /** If an entity is temporarily unavailable,
+    this instructs the client to try again later.
+    Value could be a specified period of time (in seconds) or a HTTP-date **/
+    pub retry_after: String,
+    /** A name for the server **/
+    pub server: String,
+    /** An HTTP cookie **/
+    pub set_cookie: String,
+    /** A HSTS Policy informing the HTTP client how long to cache the HTTPS only policy and whether this applies to subdomains **/
+    pub strict_transport_security: String,
+    /** The Trailer general field value indicates that the given set of header fields is present in the trailer of a message encoded with chunked transfer coding **/
+    pub trailer: String,
+    /** The form of encoding used to safely transfer the entity to the user. Currently defined methods are: chunked, compress, deflate, gzip, identity **/
+    pub transfer_encoding: String,
+    /** Tracking Status header, value suggested to be sent in response to a DNT(do-not-track), possible values:
+    "!" — under construction
+    "?" — dynamic
+    "G" — gateway to multiple parties
+    "N" — not tracking
+    "T" — tracking
+    "C" — tracking with consent
+    "P" — tracking only if consented
+    "D" — disregarding DNT
+    "U" — updated **/
+    pub tracking_status: String,
+    /** Ask the client to upgrade to another protocol **/
+    pub upgrade: String,
+    /** Tells downstream proxies how to match future request headers to decide whether the cached response can be used rather than requesting a fresh one from the origin server **/
+    pub vary: String,
+    /** Informs the client of proxies through which the response was sent. **/
+    pub via: String,
+    /** A general warning about possible problems with the entity body. **/
+    pub warning: String,
+    /** Indicates the authentication scheme that should be used to access the requested entity. **/
+    pub www_authenticate: String,
+    /** Clickjacking protection:
+    deny - no rendering within a frame,
+    sameorigin - no rendering if origin mismatch,
+    allow-from - allow from specified location,
+    allowall - non-standard, allow from any location **/
+    pub x_frame_options: String,
+    // Common non-standard response fields
+}
