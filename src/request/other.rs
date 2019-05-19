a016defab5bd4432e3c9c7e019879fff7c13c408
insomnyawolf <insomnyawolf@gmail.com>
2019-05-19 16:01:29 +0200

    Added Request Origin Info

--- src/request/other.rs
+++ src/request/other.rs
@@ -2,13 +2,16 @@ use std::net::TcpStream;
 
 #[derive(Debug, Default)]
 pub struct Other {
-    pub remore_adress: String,
+    pub remote_ip: String,
+    pub remote_port: String,
 }
 
 impl Other {
     pub fn parse(stream: TcpStream) -> Other {
+        let remote_addr = stream.peer_addr().unwrap();
         Other {
-            remore_adress: stream.peer_addr().unwrap().to_string(),
+            remote_ip: remote_addr.ip().to_string(),
+            remote_port: remote_addr.port().to_string(),
         }
     }
 }
